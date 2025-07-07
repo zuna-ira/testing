8dfce8f58faa897154336331b777a8d4b8ae4362
zuna-ira <zunairasabir544@gmail.com>
2025-07-07 23:51:47 +0500

    Update Leap year checker.cpp

--- Leap year checker.cpp
+++ Leap year checker.cpp
@@ -1,6 +1,3 @@
-//Write a program to determine if a given year is a leap year.
-//A year is a leap year if it is divisible by 4 but not divisible by 100 unless it is also divisible by 400.
-//Use the || operator to combine the conditions.
 #include<iostream>
 using namespace std;
 int main(){
