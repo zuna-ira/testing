f6ac541c4badbf1dd7002668779fbff5d121dcf6
zuna-ira <zunairasabir544@gmail.com>
2025-07-10 15:15:01 +0500

    Update Leap year checker.cpp

--- Leap year checker.cpp
+++ Leap year checker.cpp
@@ -4,6 +4,8 @@ int main(){
 	int year;
 	cout<<"Enter the number of days in a year: ";
 	cin>>year;
+	//comment
+	#bug
 	
 	if ((year%4==0 && year%100!=0) || (year%400==0)){
 		cout<<"This is a leap year!";
